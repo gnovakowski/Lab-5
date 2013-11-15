f07a919c3a51567e883190c168269e96b0a78b08
gnovakowski <c15geoffrey.novakowski@usafa.edu>
2013-11-15 13:09:10 -0700

    Create clockspeed.h

---
+++ clockspeed.h
@@ -0,0 +1,18 @@
+/*
+ * clockspeed.h
+ *
+ *  Created on: Nov 7, 2013
+ *      Author: C15Geoffrey.Novakows
+ */
+
+#include <msp430.h>
+
+#ifndef CLOCKSPEED_H_
+#define CLOCKSPEED_H_
+
+void RunAt1Hz();
+void RunAt8Hz();
+void RunAt12Hz();
+void RunAt16Hz();
+
+#endif /* CLOCKSPEED_H_ */
