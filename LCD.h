71d382292ed23a1a1abe4f285af131eb486038c7
gnovakowski <c15geoffrey.novakowski@usafa.edu>
2013-11-15 13:10:11 -0700

    Create LCD.h

---
+++ LCD.h
@@ -0,0 +1,34 @@
+/*
+ * LCD.h
+ *
+ *  Created on: Oct 21, 2013
+ *      Author: C15Geoffrey.Novakows
+ *      Description: Library for msp430 to Geekbox LCD interaction.
+ */
+
+#ifndef LCD_H_
+#define LCD_H_
+
+void initLCD();
+
+void clearLCD();
+
+void initSPI();
+
+void line1Cursor();
+
+void line2Cursor();
+
+void scrollString(char string1[], char string2[]);
+
+char* rotateString(char string[]);
+
+void writeString(char* strg2Write);
+
+void writeChar(char char2Write);
+
+void writeCommandByte(char commandByte);
+
+void writeDataByte(char dataByte);
+
+#endif /* LCD_H_ */
